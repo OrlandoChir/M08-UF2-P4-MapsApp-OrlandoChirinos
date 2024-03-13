7ab261d8fa3e0038e2981133a51483fdb1e6d350
orlandow <orlando.chirinos.7e6@itb.cat>
2024-03-13 09:49:12 +0100

    Solucionado el import.

--- app/src/main/java/orlando/p4_mapsapp_orlandochirinos/view/Map.kt
+++ app/src/main/java/orlando/p4_mapsapp_orlandochirinos/view/Map.kt
@@ -7,14 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.Divider
-import androidx.compose.material.Text
-import androidx.compose.material.rememberDrawerState
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+
 
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier

31587ba82f2692843ce8b0e6128f493693e0a8dc
orlandow <orlando.chirinos.7e6@itb.cat>
2024-04-09 13:17:49 +0200

    Firebase2 Aun no implementada la previsualizacion de la foto tomada, crashea cuando se vuelve atrás desde la galería.

--- app/src/main/java/orlando/p4_mapsapp_orlandochirinos/Models/Ubicacion.kt
+++ app/src/main/java/orlando/p4_mapsapp_orlandochirinos/Models/Ubicacion.kt
@@ -2,9 +2,16 @@ package orlando.p4_mapsapp_orlandochirinos.Models
 import com.google.android.gms.maps.model.LatLng
 
 data class Ubicacion(
-    val nombre: String,
+    val nombre: String? = null,
     val snippet: String,
     val position: LatLng,
     val tag: String,
-    val image: String
-)
+    val image: String? = null
+){
+    constructor():this(
+        nombre = null,
+        snippet = "",
+        position = LatLng(41.4534265, 2.1837151),
+        tag = "",
+        image = null )
+}
